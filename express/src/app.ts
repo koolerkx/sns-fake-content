3e508087a8fada8f66bf5c450bbfac8274e0f3cd
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2023-03-17 01:04:58 +0800

    feat: create a basic structure of the express project that act as a proxy server handling requests to the backend model server

--- express/src/app.ts
+++ express/src/app.ts
@@ -1,14 +1,21 @@
 import express from "express";
+import * as dotenv from 'dotenv';
+import process from 'process';
+import { detectionRouter } from "./route";
+import cors from 'cors';
+
+// get dotenv config
+dotenv.config()
 
 const app = express();
-const port = 3000;
-app.get("/", (req, res) => {
-  res.send("The server is working!");
-});
+const port = parseInt(process.env.PORT ?? '3000');
 
+// middleware and router
+app.use(express.json());
+app.use(cors());
+app.use(process.env.API_PREFIX || '/v1', detectionRouter);
+
+// bootstrap
 app.listen(port, () => {
-  if (port === 3000) {
-    console.log("true");
-  }
-  console.log(`server is listening on ${port} !!!`);
+    console.log(`server is listening on ${port} !!!`);
 });
