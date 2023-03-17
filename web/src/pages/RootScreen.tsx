c25a11c1b1e8fa6ecdc5be2fa7f1300170f120a2
Kooler Fan <koolerfan@oursky.com>
2023-03-17 17:04:27 +0800

    app: Add menu item to sidebar

--- web/src/pages/RootScreen.tsx
+++ web/src/pages/RootScreen.tsx
@@ -1,46 +1,47 @@
 import React, { useState } from 'react';
 import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
+  InboxOutlined,
+  HomeOutlined,
+  FileTextOutlined,
+  InfoCircleOutlined,
+  BarChartOutlined,
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Layout, Menu, theme } from 'antd';
 import { Outlet } from 'react-router-dom';
 
 const { Header, Content, Footer, Sider } = Layout;
 
 type MenuItem = Required<MenuProps>['items'][number];
 
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[]
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem;
-}
-
 const items: MenuItem[] = [
-  getItem('Option 1', '1', <PieChartOutlined />),
-  getItem('Option 2', '2', <DesktopOutlined />),
-  getItem('User', 'sub1', <UserOutlined />, [
-    getItem('Tom', '3'),
-    getItem('Bill', '4'),
-    getItem('Alex', '5'),
-  ]),
-  getItem('Team', 'sub2', <TeamOutlined />, [
-    getItem('Team 1', '6'),
-    getItem('Team 2', '8'),
-  ]),
-  getItem('Files', '9', <FileOutlined />),
+  {
+    key: 'Home',
+    icon: <HomeOutlined />,
+    label: 'Home',
+  },
+  {
+    key: 'Detection',
+    icon: <InboxOutlined />,
+    label: 'Detection',
+  },
+  {
+    key: 'Dataset',
+    icon: <FileTextOutlined />,
+    label: 'Dataset',
+    children: [
+      {
+        key: 'Information',
+        icon: <InfoCircleOutlined />,
+        label: 'Information',
+      },
+      {
+        key: 'Word Frequency',
+        icon: <BarChartOutlined />,
+        label: 'Word Frequency',
+      },
+    ],
+  },
 ];
 
 const RootScreen: React.FC = () => {
@@ -65,7 +66,7 @@ const RootScreen: React.FC = () => {
         />
         <Menu
           theme="dark"
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={['Home']}
           mode="inline"
           items={items}
         />
