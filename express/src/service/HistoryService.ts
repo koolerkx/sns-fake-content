be1fc148bddaf9afaf7c8a7aa142e7892edef627
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2023-03-29 22:54:28 +0800

    feat: add history service that memorize the detection result

---
+++ express/src/service/HistoryService.ts
@@ -0,0 +1,32 @@
+// import { History, HistoryModel } from "../schemas/History.schema";
+import { prisma } from "../utils/db";
+
+export class HistoryService {
+
+    async getAllHistory() {
+        return await prisma.histories.findMany();
+    }
+
+    async addHistory(obj: { score: number; text: string; type: string; }) {
+        return await prisma.histories.create({
+            data: {
+                ...obj,
+                createdAt: new Date(),
+                label: obj.score > .5 ? 'true' : 'false',
+            }
+        });
+    }
+
+    async removeHistory(id: string) {
+        return await prisma.histories.delete({
+            where: {
+                id,
+            }
+        });
+    }
+
+    async resetHistoryList() {
+        return await prisma.histories.deleteMany();
+    }
+
+}
