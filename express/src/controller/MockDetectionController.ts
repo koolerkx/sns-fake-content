1e473dee23f4367696c5a3709496cdd44ea7de12
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2023-03-29 22:56:26 +0800

    feat: create a mock detection controller that sends no request to the AWS

---
+++ express/src/controller/MockDetectionController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import { HistoryService } from '../service/HistoryService';
+
+export class MockDetectionController {
+    constructor(
+        private historyService: HistoryService = new HistoryService(),
+    ) { }
+
+    detect = async (req: Request, res: Response): Promise<void> => {
+        const { type, text } = req.body;
+
+        if (!type || !text) {
+            throw new Error('Invalid request body');
+        }
+
+        const score = Math.random();
+
+        await this.historyService.addHistory({
+            type,
+            text,
+            score,
+        });
+
+        res.json({
+            result: true,
+            data: score,
+        });
+    }
+}
