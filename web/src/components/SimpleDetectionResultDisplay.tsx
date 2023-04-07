6908767631832f2aac676758e102e709f6cfe2a8
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2023-04-07 08:20:25 +0800

    fix: make use of gutter and align the margin values

--- web/src/components/SimpleDetectionResultDisplay.tsx
+++ web/src/components/SimpleDetectionResultDisplay.tsx
@@ -12,16 +12,12 @@ const SimpleDetectionResultDisplay = () => {
         return ['success', 'error'][Math.floor((1 - score) * 2)] as AlertType;
     }, [score]);
 
-    useEffect(() => {
-        console.log(text);
-    }, [text]);
-
     if (!text) return <></>;
 
     return (
         <Alert
             type={type}
-            message={`The creditability of this message is ${(score * 100).toFixed(2)}% to be true.`}
+            message={`The creditability of this message is ${(score * 100).toFixed(2)}%.\n Hence It should be a ${score > .5} message.`}
         />
     );
 }
