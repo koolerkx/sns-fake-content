8461885d6cf39ca1990516a91783cef69555d382
Kooler Fan <kmcazee@gmail.com>
2023-04-11 04:19:19 +0800

    Implement transformer model

--- backend_api/model_process/xlnet.py
+++ backend_api/model_process/xlnet.py
@@ -1,10 +1,24 @@
-from utils.str_to_number import str_to_number
+import os
+from transformers import XLNetForSequenceClassification, XLNetTokenizer
+from scipy.special import softmax
+
 
 class XLNetModelWrapper:
     MODEL_NAME = "XLNET"
-
-    def load(self):
-        pass
+    checkpoint = os.path.abspath(f"model_file/xlnet/checkpoint")
+    tokenizer = os.path.abspath(f"model_file/xlnet/tokenizer")
 
     def detect(self, text: str):
-        return str_to_number(text)
+        model = XLNetForSequenceClassification.from_pretrained(self.checkpoint)
+        tokenizer = XLNetTokenizer.from_pretrained("xlnet-base-cased")
+        
+        model_inputs = tokenizer(
+            text,
+            padding="max_length",
+            truncation=True,
+            max_length=128,
+            return_tensors="pt",
+        )
+
+        output = model(model_inputs["input_ids"])
+        return softmax(output["logits"].detach().numpy())[0][1]
