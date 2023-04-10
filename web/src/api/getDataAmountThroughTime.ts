bc09056f5ec8afaa7f4cd69b4cf88cee9c484994
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2023-04-11 07:37:56 +0800

    feat: inject label variable into the url params

--- web/src/api/getDataAmountThroughTime.ts
+++ web/src/api/getDataAmountThroughTime.ts
@@ -1,5 +1,7 @@
-export default async () => {
-	const res = await fetch(`${import.meta.env.VITE_API_ENDPOINT}/aggregation/getDataAmountThroughTime`);
+export default async (label: "true" | "false" | null = null) => {
+	const res = await fetch(`${import.meta.env.VITE_API_ENDPOINT}/aggregation/getDataAmountThroughTime?` + new URLSearchParams(label && {
+    label: label ?? "",
+  } || {}));
     return await res.json() as {
         label: "true" | "false",
         created_at: number,
--- web/src/api/getLabelDistributionThroughTime.ts
+++ web/src/api/getLabelDistributionThroughTime.ts
@@ -1,5 +1,7 @@
-export default async () => {
-	const res = await fetch(`${import.meta.env.VITE_API_ENDPOINT}/aggregation/getLabelDistributionThroughTime`);
+export default async (label: "true" | "false" | null = null) => {
+	const res = await fetch(`${import.meta.env.VITE_API_ENDPOINT}/aggregation/getLabelDistributionThroughTime?` + new URLSearchParams(label && {
+    label: label ?? "",
+  } || {}));
     return await res.json() as {
         label: string,
         created_at: number,
